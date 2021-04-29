38597f4fec098819bfda753388e4f53ce4ad104f
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-04-29 19:55:58 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,22 +4,15 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include "catch.hpp"
+#include <assert.h>
 
 
-
-TEST_CASE(" detect the ranges and readings in each range") {
+int main()
+{
 	std::vector<int> currentSamples = { 1, 3, 5, 4, 2,27,29,28,30, 10, 11, 12,99 };
 	currentReport* current_Report = new currentReport(currentSamples);
 	current_Report->generateGroup();
 	current_Report->reportGroup();
-  // REQUIRE(current_Report->CurrentGroupData.size() == 4 );
-}
-
-TEST_CASE(" detect the ranges and readings in each range") {
-	std::vector<int> currentSamples = { 1, 3, 5 };
-	currentReport* current_Report = new currentReport(currentSamples);
-	current_Report->generateGroup();
-	current_Report->reportGroup();
-   //REQUIRE(current_Report->CurrentGroupData[0].unitsInGroup == 5);
+	assert(current_Report->CurrentGroupData.size() == 4);
+   	return 0;
 }
