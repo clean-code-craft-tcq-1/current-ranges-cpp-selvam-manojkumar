83186d05a8013bd40857d168b74a9795a52a3958
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-04-29 19:39:09 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,25 @@
+
+#include "currentRanges.h"
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include "catch.hpp"
+
+
+
+TEST_CASE(" detect the ranges and readings in each range") {
+	std::vector<int> currentSamples = { 1, 3, 5, 4, 2,27,29,28,30, 10, 11, 12,99 };
+	currentReport* current_Report = new currentReport(currentSamples);
+	current_Report->generateGroup();
+	current_Report->reportGroup();
+   REQUIRE(current_Report->CurrentGroupData.size() == 4 );
+}
+
+TEST_CASE(" detect the ranges and readings in each range") {
+	std::vector<int> currentSamples = { 1, 3, 5 };
+	currentReport* current_Report = new currentReport(currentSamples);
+	current_Report->generateGroup();
+	current_Report->reportGroup();
+   REQUIRE(current_Report->CurrentGroupData[0].unitsInGroup == 5);
+}
